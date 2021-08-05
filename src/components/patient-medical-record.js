fe28455ff574ff3bcfa94b43e070c8e7b338cda7
Taz Tony <tazutony@gmail.com>
2021-08-05 17:06:21 -0400

    search bar

--- src/components/patient-medical-record.js
+++ src/components/patient-medical-record.js
@@ -17,8 +17,14 @@ export default class PatientMedicalRecord extends Component {
         return (
           <div className="container">
             <header className="jumbotron">
-              <h3>{this.state.content}</h3>
-            </header>
+            <div className="ui search">
+               <div className="ui icon input">
+                  <input type="text" style={{width: '250px', border: '2px solid powderblue', fontFamily:'times new roman'}} placeholder="Search" className="prompt" />
+                  <i className="search icon"></i>
+
+               </div>
+            </div>
+            </header>    
           </div>
         );
       }
