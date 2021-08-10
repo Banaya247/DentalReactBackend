0fb04565ae3f07f18a9a505afea8e829a4db78cb
Tanzim Kazi <tanzimk96@gmail.com>
2021-08-10 16:00:33 -0400

    created scheduler for availibility-- there is no functionality

--- src/components/availability.component.js
+++ src/components/availability.component.js
@@ -1,40 +1,52 @@
 import React, { Component } from "react";
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+    Scheduler,
+    DayView,
+    Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+
 
 import UserService from "../services/user.service";
+
+const currentDate = '2021-08-10';
+const schedulerData = [
+    { startDate: '2021-08-10T09:45', endDate: '2021-08-10T11:00', title: 'John Doe Cleaning' },
+    { startDate: '2021-08-10T12:00', endDate: '2021-08-10T13:30', title: 'Jane Doe Checkup' },
+];
 
 export default class Availability extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            content: ""
+            content: "Doctor's Availability"
         };
     }
 
-    componentDidMount() {
-        UserService.getPublicContent().then(
-            response => {
-                this.setState({
-                    content: response.data
-                });
-            },
-            error => {
-                this.setState({
-                    content:
-                        (error.response && error.response.data) ||
-                        error.message ||
-                        error.toString()
-                });
-            }
-        );
-    }
 
     render() {
         return (
             <div className="container">
-                <header className="jumbotron">
-                    <h3>{this.state.content}</h3>
-                </header>
+                <h5>{this.state.content}</h5>
+                <div>
+                    <Paper>
+                        <Scheduler
+                            data={schedulerData}
+                        >
+                            <ViewState
+                                currentDate={currentDate}
+                            />
+                            <DayView
+                                startDayHour={9}
+                                endDayHour={17}
+                            />
+                            <Appointments />
+                        </Scheduler>
+                    </Paper>
+                </div>
             </div>
         );
     }
